9cb8d692761a35f88416e9e51170a080742e403a
robPTY <rcadown8@gmail.com>
2024-09-22 19:57:10 -0500

    fixed navbar styling and landing page styling, more to come

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,10 +4,12 @@ import morphdLogo from "../../assets/morphd_logo.png";
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="logoHolder">
-      <a>
-        <img src={morphdLogo}></img>
-      </a>
+    <div className="navbarHolder">
+      <div className="logoHolder">
+        <a>
+          <img src={morphdLogo}></img>
+        </a>
+      </div>
     </div>
   );
 };
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar/navbar";
 
 export const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="landingPageBody">
       <Navbar></Navbar>
     </div>
   );
