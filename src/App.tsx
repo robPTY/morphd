2f206e6bada70ea475ecda334810e3a4d724fb60
robPTY <rcadown8@gmail.com>
2024-10-02 00:33:05 -0500

    fixed auth login

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing/landing";
+import { AuthProvider } from "./auth/authContext";
 import Documentation from "./pages/documentation/documentation";
 import Resources from "./pages/resources";
 import SignIn from "./pages/signin/signin";
 import SignUp from "./pages/signUp/signup";
+import Library from "./pages/library/library";
 import ResetPassword from "./pages/resetPassword/resetPassword";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/documentation" element={<Documentation />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/library" element={<Library />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
